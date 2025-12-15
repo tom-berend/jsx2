4b0d47f48ec429860ca15cfb73d16562bea69aeb
Tom Berend <tom.berend@cheeseandcrackers.ca>
2025-12-15 16:35:19 -0500

    converted Board

--- src/indextests.ts
+++ src/indextests.ts
@@ -1,6 +1,5 @@
-import  Board  from "./base/board.js";
-import  JSXGraph  from "./jsxgraph.js"
-import  SVGRenderer  from "./renderer/svg.js"
+import { Board } from "./base/board.js";
+import JSXGraph from "./jsxgraph.js"
 
 
 export class IndexTests {
@@ -31,18 +30,18 @@ export class IndexTests {
 
     point() {
         let a = this.newBoard.create('point', [1, 3])
-        let a2 = this.newBoard.create('point', [()=>a.X()+1, ()=>a.Y()+1],{name:'new second'})
+        let a2 = this.newBoard.create('point', [() => a.X() + 1, () => a.Y() + 1], { name: 'new second' })
 
         let b = this.oldBoard.create('point', [1, 3])
-        let b2 = this.oldBoard.create('point', [()=>b.X()+1, ()=>b.Y()+1],{name:'old second'})
+        let b2 = this.oldBoard.create('point', [() => b.X() + 1, () => b.Y() + 1], { name: 'old second' })
     }
     text() {
         let a = this.newBoard.create('text', [2, 3, "test"], { name: 'foo', strokecolor: 'red' })
         let b = this.oldBoard.create('text', [2, 3, "test"], { name: 'foo', strokecolor: 'red' })
     }
     line() {
-        let a = this.newBoard.create('segment', [[-1, -1],[-2,-1]] , { strokecolor: 'red' })
-        let b = this.oldBoard.create('segment', [[-1, -1],[-2,-1]] , { strokecolor: 'red' })
+        let a = this.newBoard.create('segment', [[-1, -1], [-2, -1]], { strokecolor: 'red' })
+        let b = this.oldBoard.create('segment', [[-1, -1], [-2, -1]], { strokecolor: 'red' })
 
         // this.newBoard.create('circle', [[-1, -1],[-2,-1]] , { strokecolor: 'green' })
         // this.oldBoard.create('circle', [[-1, -1],[-2,-1]] , { strokecolor: 'green' })
